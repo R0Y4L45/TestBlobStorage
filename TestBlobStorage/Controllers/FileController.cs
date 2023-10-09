446a687d6df0f3b54bcdd51ee04799ba4a9e84b8
bele_fl83 <bele_fl83@STHQ0118-02>
2023-10-09 21:28:16 +0400

    Add project files.

---
+++ TestBlobStorage/Controllers/FileController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.NetworkInformation;
+using TestBlobStorage.Services;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestBlobStorage.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileController : ControllerBase
+{
+    private readonly IStorageManager _storageManager;
+
+    public FileController(IStorageManager storageManager)
+    {
+        _storageManager = storageManager;
+    }
+
+    [HttpGet("getUrl")]
+    public IActionResult GetUrl(string fileName)
+    {
+        try
+        {
+            var result = _storageManager.GetSignedUrl(fileName);
+            return Ok(result);
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.NotFound);
+        }
+    }
+
+    [HttpGet("deleteUrl")]
+    public IActionResult DeleteUrl(string fileName)
+    {
+        try
+        {
+            var result = _storageManager.DeleteFile(fileName);
+            return Ok(result);
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.NotFound);
+        }
+    }
+    [HttpPost("UploadUrl")]
+    public IActionResult UploadUrl(IFormFile formFile)
+    {
+        try
+        {
+
+            return Ok(_storageManager.UploadFile(formFile));
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ TestBlobStorage/Services/BlobStorageManager.cs
@@ -0,0 +1,110 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using Microsoft.Extensions.Options;
+using TestBlobStorage.Utilities;
+
+namespace TestBlobStorage.Services;
+
+public class BlobStorageManager : IStorageManager
+{
+    private readonly BlobStorageOptions _storageOptions;
+
+    public BlobStorageManager(IOptions<BlobStorageOptions> options)
+    {
+        _storageOptions = options.Value;
+    }
+
+    public bool DeleteFile(string fileName)
+    {
+        BlobContainerClient client = new BlobContainerClient(_storageOptions.ConnectionString, _storageOptions.ContainerName);
+        BlobClient file = client.GetBlobClient(fileName);
+
+        try
+        {
+            file.Delete();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteFileAsync(string fileName)
+    {
+        BlobContainerClient client = new BlobContainerClient(_storageOptions.ConnectionString, _storageOptions.ContainerName);
+        BlobClient file = client.GetBlobClient(fileName);
+
+        try
+        {
+            await file.DeleteAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public string GetSignedUrl(string fileName)
+    {
+        var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+        var containerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+        var blobClient = containerClient.GetBlobClient(fileName);
+
+        var signedUrl = blobClient.GenerateSasUri(BlobSasPermissions.Read, DateTime.UtcNow.AddHours(2)).AbsoluteUri;
+
+        return signedUrl;
+    }
+
+    public async Task<string> GetSignedUrlAsync(string fileName)
+    {
+        var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+        var containerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+        var blobClient = containerClient.GetBlobClient(fileName);
+
+        var signedUrl = blobClient.GenerateSasUri(BlobSasPermissions.Read, DateTime.UtcNow.AddHours(2)).AbsoluteUri;
+
+        return await Task.FromResult(signedUrl);
+    }
+
+    public bool UploadFile(IFormFile formFile)
+    {
+        
+        BlobContainerClient containerClient = new BlobContainerClient(_storageOptions.ConnectionString, _storageOptions.ContainerName);
+        BlobClient blobClient = containerClient.GetBlobClient(formFile.FileName);
+
+        try
+        {
+            blobClient.Upload(formFile.OpenReadStream(), new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders { ContentType = formFile.ContentType }
+            });
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> UploadFileAsync(Stream stream, string fileName, string contentType)
+    {
+        BlobContainerClient containerClient = new BlobContainerClient(_storageOptions.ConnectionString, _storageOptions.ContainerName);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+        try
+        {
+            await blobClient.UploadAsync(stream, new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders { ContentType = contentType }
+            });
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
